acded792a20bca74928d77c3fd8b9f7727539e8b
Rocilene Paiva da Silva Costa <rpscosta@inf.ufpr.br>
2018-03-03 15:29:24 -0300

    Iniciando repositório de scripts em R

---
+++ aula1.R
@@ -0,0 +1,46 @@
+
+## Funções e argumentos
+## não há obrigatoriedade de setar o nome dos argumentos,
+## mas é uma boa prática setar o nome dos demais exemplo: runinf(n, min, max)
+## há funções com argumentos obrigatórios, fique atento!
+## dica: informa os argumentos da função -->> args(runif)
+## dica: apropos("Digite o termo") abre uma pesquisa em todos os pacotes instalados
+## help.search("Digite o termo")
+
+
+runif(10)
+
+runif(10, min=2, max=10)
+
+#mostra pacotes instalados
+search()
+
+##instala pacotes que vem pré-carregados
+libary()
+
+##instala pacotes diretamente da internet *consultar pacotes disponíveis
+install.packages("mvtnorm")
+
+##exibe pacotes instalados e/ou pacotes que precisam ser atualizados
+packageStatus()
+
+## Exemplo criando uma função
+ola.mundo <- function(){
+  writeLines("Olá Mundo!")
+}
+
+## Função com um argumento especificado
+ola.mundo <- function(texto){
+  writeLines(texto)
+}
+
+## Na chamada o argumento obrigatório deve ser passado
+ola.mundo(texto = "Olá mundo!")
+
+
+
+
+
+
+
+
---
+++ exercicio1.R
@@ -0,0 +1,37 @@
+## Usando a função runinf() gere 30 numeros aleatórios entre:
+
+## 0 e 1
+runif(30, min = 0, max = 1)
+
+## -5 e 5
+runif(30, min = -5, max = 5)
+
+## 10 e 500
+runif(30, min = 10, max = 500)
+
+## Alternando a posição dos argumentos
+runif( max = 500, 30, min = 10)
+runif( min = 10, max = 500, 30)
+
+##veja o help da função(?)
+?runif
+?"+"
+
+## Crie uma função para fazer a soma de dois numeros
+soma.numeros <- function (num1, num2){
+  num1+num2
+}
+soma.numeros(3,5)
+
+## criar função que simula a jogada de um dado
+dado <- function (){
+  # sample(x = 1:6, size = 1)
+}
+dado()
+
+## criar função que simula a jogada de dois dados
+dados <- function (){
+  sample(x = 1:6, size = 2, replace = TRUE)
+}
+dados()
+
